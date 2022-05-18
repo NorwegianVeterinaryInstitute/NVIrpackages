cad2b430f107a36c5d23c788c993c359ba1b38b2
Petter Hopp <petter.hopp@vetinst.no>
2022-05-18 14:01:26 +0200

    chore: develop.R includes argument manual = "update" in document_NVIpkg

--- notes/develop.R
+++ notes/develop.R
@@ -23,23 +23,14 @@ pkg <- stringi::stri_extract_last_words(pkg_path)
 NVIpackager::document_NVIpkg(pkg = pkg,
                              pkg_path = pkg_path,
                              style = TRUE,
-                             contributing = TRUE,
-                             readme = TRUE,
+                             contributing = FALSE,
+                             readme = FALSE,
+                             manual = "update",
                              scope = c("spaces", "line_breaks"))
 
 
 # spelling::spell_check_package(vignettes = TRUE, use_wordlist = TRUE)
 
-
-# Alternative for creating the PDF-manual. The manual is not put in the correct directory
-# system(paste(shQuote(file.path(R.home("bin"), "R")),
-#              "CMD",
-#              "Rd2pdf",
-#              paste0("../", pkg)))
-# file.copy(from = paste0(pkg, ".pdf"), to = "./vignettes", overwrite = TRUE)
-# file.remove(".Rd2pdf16372")
-# file.remove("NVIdb.pdf")
-# check .install_extras
 
 # TEST PACKAGE ----
 # Run tests included in ./tests.
