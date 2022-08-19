3fad5df8e65717a3549c8dce608a714a41f64fe1
Petter Hopp <petter.hopp@vetinst.no>
2022-08-19 18:11:17 +0200

    doc: Improved description of the data in NVIpackages

--- R/data.R
+++ R/data.R
@@ -1,15 +1,18 @@
-#' @title Data: Description of the NVIverse packages .
+#' @title Data: Description of the `NVIverse` packages .
 #'
-#' @description A data frame with key information for the NVIverse packages. The
-#'     information is mainly used as input to \code{CONTRIBUTING.md} and \code{README.Rmd}.
-#' @details The data frame is generated in \code{./data-raw/generate_NVIpackages.R}.
-#'     Edit this file to update the data frame with changes in the NVIverse packages.
+#' @description A data frame listing the `NVIverse` packages with some meta data.
+#'     The information is used  in `NVIbatch` in functions that use a list of
+#'     `NVIverse` packages as input and in all `NVIverse` packages to generate
+#'     the text listing the `NVIverse` packages in the documents `CONTRIBUTING.md`
+#'     and `README.Rmd`.
+#' @details The data frame is generated in "./data-raw/generate_NVIpackages.R".
+#'     Edit this file to update the data frame with changes in the `NVIverse` packages.
 #'
 #' @format A data frame with 3 variables:
 #' \describe{
-#'   \item{Package}{name of the NVIverse package.}
+#'   \item{Package}{name of the `NVIverse` package.}
 #'   \item{Status}{Whether the package is "Private" or "Public".}
-#'   \item{Description}{The short description of the package as given in the "Title" field in \code{DESCRIPTION}.}
+#'   \item{Description}{The short description of the package as given in the "Title" field in `DESCRIPTION`.}
 #' }
-#' @source \code{./data-raw/generate_NVIpackages.R} in package \code{NVIpackager}
+#' @source "./data-raw/generate_NVIpackages.R" in package `NVIrpackages`.
 "NVIpackages"
